ee9fdbcc507ed8110c68ebb59ac35ecb2b5da83c
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-03-25 23:20:17 +0900

    Add merge sort

--- src/main/java/io/github/fabasoad/sorting/SortingTest.java
+++ src/main/java/io/github/fabasoad/sorting/SortingTest.java
@@ -5,9 +5,9 @@ import java.util.Arrays;
 public class SortingTest {
 
     public static void main(String[] args) {
-        var arr = new int[] { 1, 34, 2, 33, 3, 6 };
-        insertionSort(arr);
-        System.out.println(Arrays.toString(arr));
+        var arr = new int[] { 1, 34, 2, 33, 3, 6, 66, 67, 37 };
+        var arr1 = mergeSort(arr);
+        System.out.println(Arrays.toString(arr1));
     }
 
     // O(n^2)
@@ -37,6 +37,43 @@ public class SortingTest {
                     arr[j - 1] = temp;
                 }
             }
+        }
+    }
+
+    private static int[] mergeSort(int[] a) {
+        return mergeSort(0, a.length, a);
+    }
+
+    private static int[] mergeSort(int s, int e, int[] a) {
+        if (e - s > 1) {
+            int[] left = mergeSort(s, (s + e)/2, a);
+            int[] right = mergeSort((s + e)/2, e, a);
+            int[] res = new int[left.length + right.length];
+            int l = 0, r = 0, i = 0;
+            while (l < left.length && r < right.length) {
+                if (left[l] < right[r]) {
+                    res[i] = left[l];
+                    i++;
+                    l++;
+                } else {
+                    res[i] = right[r];
+                    i++;
+                    r++;
+                }
+            }
+            while (l < left.length) {
+                res[i] = left[l];
+                i++;
+                l++;
+            }
+            while (r < right.length) {
+                res[i] = right[r];
+                i++;
+                r++;
+            }
+            return res;
+        } else {
+            return new int[] { a[s] };
         }
     }
 }
